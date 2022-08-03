c02c345e48fb04a89bd502140570370273b09c4f
martins <martin.santamaria@rga.com>
2022-08-03 19:19:00 -0300

    Added POST, PUT, PATCH and DELETE methods to our APIView

--- src/app/urls.py
+++ src/app/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+    path('api/profiles/', include('profiles.urls'))
 ]
